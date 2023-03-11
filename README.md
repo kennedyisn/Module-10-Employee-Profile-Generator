b4850e7c77ad2460cc73037e912fb65a2ff5c215
kennedyisn <kennedyhayles@gmail.com>
2023-03-11 18:06:06 -0500

    Added Readme, finalized prompts, sample webpage

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Module-10-Employee-Profile-Generator
+# <Module-9-ReadME-File-Maker>
+
+## Description
+
+This project was created to save managers time when creating a webpage to hold the information of seperate teams that they are in charge of. The webpage created displays all team members relavent information cleary while also hyperlinking github usernames and emails, making it that quicker and easier to review someones code or send them a message about an important update. In creating this project I leanred more about the possibilities of node in creating different files or applications based off of user input from a terminal.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Badges](#badges)
+
+## Installation
+
+Before running this application you need to install "inquirer" and "fs" which can be done by typing "npm install inquirer" followed by "npm install fs" in the terminal.
+
+## Usage
+
+After installing required dependencies, open the terminal and type in "node index.js" to run the application. You will then be prompted questions that will be used to form your team managers profile, after that you will encounter a menu allowing you to create a new engineer profile, intern profile, or finish building your team and see your created team profile webpage. 
+
+## Credits
+
+https://www.codecademy.com/article/getting-user-input-in-node-js
+
+
+## License
+
+The MIT License
+
+## Badges
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- index.js
+++ index.js
@@ -6,97 +6,138 @@ const Manager = require('.lib/Manager');
 const Engineer = require('.lib/Engineer');
 const Intern = require('.lib/Intern');
 
-// User input to create Employee profile
-const baseEmployeeQuestions = [
+//using init to start first function
+
+
+// User input to create Manager profile
+function managerCreator() {
+  inquirer.prompt([
     {
       type: 'input',
       message: 'What is the name of your team manager?',
       name: 'name',
       validate: (value) => { if(value){return true} else {return 'Please enter the name of your team manager.'}}
     },
+
     {
       type: 'input',
       message: 'Enter the employee ID number for your team manager.',
       name: 'id',
       validate: (value) => { if(value){return true} else {return 'Please enter the employee ID number of your team manager.'}}
     },
-    
+
     {
       type: 'input',
       message: 'Enter the email address for your team manager.',
       name: 'email',
       validate: (value) => { if(value){return true} else {return 'Please enter the email address of your team manager to continue.'}}
     },
-  
+
     {
       type: 'input',
       message: 'What is the office number for your team manager?',
       name: 'officeNumber',
       validate: (value) => {if(value){return true} else {return 'Please enter the office number of your team manager to continue.'}}
     },
-    
-    {
-      type: "list",
-      message: "Select how you would like to proceed.",
-      name: "choice",
-      choices: ['create new engineer profile', 'create new intern profile', 'finish building team'],
-      validate: (value) => { if(value){return true} else {return 'Please select an option to continue. If your project has no license select the N/A option.'}}
-    },
-];
+  ])
+  .then((data) => {
+    const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+    employees.push(manager);
+    console.log("Team: ", employees)
+    teamCreator()
+  });
+}
 
-const engineerQuestions = [
-    {
-      type: 'input',
-      message: 'What is the name of this engineer?',
-      name: 'name',
-      validate: (value) => { if(value){return true} else {return 'Please enter the name of this engineer.'}}
-    },
-    {
-      type: 'input',
-      message: 'Enter the employee ID number for this engineer.',
-      name: 'id',
-      validate: (value) => { if(value){return true} else {return 'Please enter the employee ID number of this engineer.'}}
-    },
+// User input to create the rest of the profiles
+function teamCreator() {
+  inquirer.prompt([ 
+  {
+    type: "list",
+    message: "Select how you would like to proceed.",
+    name: "choice",
+    choices: ['Create new engineer profile', 'Create new intern profile', 'Finish building team'],
+    validate: (value) => { if(value){return true} else {return 'Please select an option to continue.'}}
+  },
+  ])
+  .then((answer) => {
+    if (answer.choice === 'Create new engineer profile') { 
+      return inquirer.prompt([
+      {
+        type: 'input',
+        message: 'What is the name of this engineer?',
+        name: 'name',
+        validate: (value) => { if(value){return true} else {return 'Please enter the name of this engineer.'}}
+      },
+
+      {
+        type: 'input',
+        message: 'Enter the employee ID number for this engineer.',
+        name: 'id',
+        validate: (value) => { if(value){return true} else {return 'Please enter the employee ID number of this engineer.'}}
+      },  
+
+      {
+        type: 'input',
+        message: 'Enter the email address for this engineer.',
+        name: 'email',
+        validate: (value) => { if(value){return true} else {return 'Please enter the email address of this engineer to continue.'}}
+      },  
+
+      {
+        type: 'input',
+        message: 'What is the github username for this engineer?',
+        name: 'github',
+        validate: (value) => {if(value){return true} else {return 'Please enter the github username for this engineer to complete their profile.'}}
+      },
+      ])
+      .then((data) => {
+      const engineer = new Engineer(data.name, data.id, data.email, data.github);
+      employees.push(engineer);
+      console.log("Team: ", employees)
+      teamCreator()
+    })
+  } else if (answer.choice === 'Create new intern profile') {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        message: 'What is the name of this intern?',
+        name: 'name',
+        validate: (value) => { if(value){return true} else {return 'Please enter the name of this intern.'}}
+      },
+
+      {
+        type: 'input',
+        message: 'Enter the employee ID number for this intern.',
+        name: 'id',
+        validate: (value) => { if(value){return true} else {return 'Please enter the employee ID number of this intern.'}}
+      },
+
+      {
+        type: 'input',
+        message: 'Enter the email address for this intern.',
+        name: 'email',
+        validate: (value) => { if(value){return true} else {return 'Please enter the email address of this intern to continue.'}}
+      },
+
+      {
+        type: 'input',
+        message: 'What is the school attended this intern?',
+        name: 'school',
+        validate: (value) => {if(value){return true} else {return 'Please enter the school attended by this intern to complete their profile.'}}
+      },
+    ])
+        .then((data) => {
+        const intern = new Intern(data.name, data.id, data.email, data.school);
+        employees.push(intern);
+        console.log("Team: ", employees)
+        teamCreator()
+      });
+  } else if (answer.choice === 'Finish building team') {
+    // save all data
     
-    {
-      type: 'input',
-      message: 'Enter the email address for this engineer.',
-      name: 'email',
-      validate: (value) => { if(value){return true} else {return 'Please enter the email address of this engineer to continue.'}}
-    },
-  
-    {
-      type: 'input',
-      message: 'What is the github username for this engineer?',
-      name: 'github',
-      validate: (value) => {if(value){return true} else {return 'Please enter the github username for this engineer to complete their profile.'}}
-    },
-]
-const internQuestions = [
-    {
-      type: 'input',
-      message: 'What is the name of this intern?',
-      name: 'name',
-      validate: (value) => { if(value){return true} else {return 'Please enter the name of this intern.'}}
-    },
-    {
-      type: 'input',
-      message: 'Enter the employee ID number for this intern.',
-      name: 'id',
-      validate: (value) => { if(value){return true} else {return 'Please enter the employee ID number of this intern.'}}
-    },
-    
-    {
-      type: 'input',
-      message: 'Enter the email address for this intern.',
-      name: 'email',
-      validate: (value) => { if(value){return true} else {return 'Please enter the email address of this intern to continue.'}}
-    },
-  
-    {
-      type: 'input',
-      message: 'What is the school attended this intern?',
-      name: 'school',
-      validate: (value) => {if(value){return true} else {return 'Please enter the school attended by this intern to complete their profile.'}}
-    },
-];
+    // push input to HTML generator
+  }
+})
+}
+
+init();
